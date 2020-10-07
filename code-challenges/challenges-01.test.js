e4a366b64a8cffc5cff7c322ec1120c9c4412896
Michael Campbell <cam3000@gmail.com>
2020-10-06 17:37:42 -0700

    almost done with test 2

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -42,9 +42,11 @@ const addValues = (arr, value) => {
 
 const addNumbers = (num, arr, times, callback) => {
   // Solution code here...
-  for(let i = 0; i < times; i++){
+  for (let i = 0; i < times; i++) {
     callback(arr, num);
   }
+  console.log(arr);
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
