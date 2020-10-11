f6c8e4255c0f6d76183e539c493627205d936ab9
Michael Campbell <cam3000@gmail.com>
2020-10-11 14:52:12 -0700

    completed challenge three

--- code-challenges/challenge-03-sort/challenges-03.test.js
+++ code-challenges/challenge-03-sort/challenges-03.test.js
@@ -53,7 +53,8 @@ For example, ['Alphabet', 'Zebra', 'alphabet', 'carrot'] is correctly sorted.
 ------------------------------------------------------------------------------------------------ */
 
 const alphabetize = (arr) => {
-  // Solution code here...
+  arr.sort();
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -64,6 +65,16 @@ Write a function named sortByLength that takes in an array of strings and return
 
 const sortByLength = (arr) => {
   // Solution code here...
+  arr.sort((a, b) => {
+    if (a.length > b.length) {
+      return 1;
+    } else if (a.length < b.length) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
