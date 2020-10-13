80d19a7c1c85dab44d01d180dab3c7f8d4ae8beb
Michael Campbell <cam3000@gmail.com>
2020-10-12 20:59:51 -0700

    added first line of code

--- code-challenges/challenges-06.test.js
+++ code-challenges/challenges-06.test.js
@@ -71,6 +71,8 @@ let $ = createSnippetWithJQuery(`
 
 const templatingWithMustache = () => {
   // Solution code here...
+  let array = [];
+  
 };
 
 /* ------------------------------------------------------------------------------------------------
