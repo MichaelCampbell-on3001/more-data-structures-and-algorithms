5fc80d80c2ef7e2e385e7d88b48ce1275e19458f
Michael Campbell <cam3000@gmail.com>
2020-10-26 20:10:43 -0700

    fixed syntax error in challenge 1

--- code-challenges/challenges-05.test.js
+++ code-challenges/challenges-05.test.js
@@ -38,7 +38,7 @@ let $ = createSnippetWithJQuery(`
 
 const templateWithJQuery = () => {
   // Solution code here...
-  let starWarsTemplate = $('#templte').html();
+  let starWarsTemplate = $('#template').html();
   starWarsPeople.forEach(value => {
     const $newSection = $(`<section>${starWarsTemplate}</section>`);
     $newSection.find('h2').text(value.name);
